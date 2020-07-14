785911d2ff25f97f0b92b5cee7f655bbd088456b
SparshAgrawal1 <56772551+SparshAgrawal1@users.noreply.github.com>
2020-07-14 15:16:27 +0530

    Updates

---
+++ precache-manifest.31b16755d6aae86f932c1196e432abc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5601313cc7f246b204476a4f2759e51",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "92f40ce144baaaf254cc",
+    "url": "/monster-rolodex/static/css/main.be449386.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f40ce144baaaf254cc",
+    "url": "/monster-rolodex/static/js/main.d6fe81f8.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
